09eb1b64c245f6a1a4aafb1b1a4f25ace26da418
Raul Silvério <rjosesilverio@gmail.com>
2019-11-01 21:54:23 -0300

    Removido bug de busca para jogos futuros

--- server/data_science/searchinggames.py
+++ server/data_science/searchinggames.py
@@ -1,11 +1,14 @@
 import pymongo
 import pandas as pd
+from crawler.mongo_connection import db
+import numpy as np
+
+import time
 
 # Conectar ao Servidor
 myclient = pymongo.MongoClient()
 
 # Utilizar o banco de dados dentro do servidor
-db = myclient["futebol"]
 data = db.brazilian_league.find()
 df = pd.DataFrame(data=data)
 # print(df.head())
@@ -16,10 +19,13 @@ def searching_last_game(team_name):
     # Cria um novo dataframe apenas com o time de interesse
     new_df = df[df['team_1'] == team_name]
 
+    # Remove a string no final do dataframe
+    new_df = new_df[new_df.date != '-']
+
     # Coloca em ordem do mais antigo para o mais novo a tabela, de acordo com a data do jogo
-    new_df = new_df.sort_values(by='date', ascending=True)
+    new_df = new_df.sort_values(by=['date'], ascending=True)
 
     # Print do Resultado esperado
-    print(f'Resultado: {str(new_df.iloc[-1, 1])} {str(new_df.iloc[-1, 9])} {str(new_df.iloc[-1, 4])} x {str(new_df.iloc[-1, 5])} {str(new_df.iloc[-1,10])}')
+    print(f'Resultado: {str(new_df.iloc[-1, 1])} {str(new_df.iloc[-1, 9])} {str(new_df.iloc[-1, 4])} x {str(new_df.iloc[-1, 5])} {str(new_df.iloc[-1, 10])}')
 
-print(searching_last_game('Palmeiras'))
+print(searching_last_game('Corinthians'))
