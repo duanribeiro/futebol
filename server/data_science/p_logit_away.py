2019364e5c941be3185ba3c95d404f23f795cc49
carloshmsimao <machado_simao@hotmail.com>
2019-11-24 16:59:57 -0300

    Subo o novo modelo de probabilidade, assim como as funções, que geram o modelo.

---
+++ server/data_science/p_logit_away.py
@@ -0,0 +1,54 @@
+import pymongo
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Conectar ao Servidor
+myclient = pymongo.MongoClient("mongodb+srv://write_user:write_pass@futebol-iwbwh.mongodb.net/test?retryWrites=true&w=majority")
+
+# Utilizar o banco de dados dentro do servidor
+db = myclient["futebol"]
+
+def probability_away():
+
+    all_games = list(
+        db.brazilian_league.find({"score_team_1": {"$ne": "-"}},
+                                 {"_id": 0, "team_2": 1, "score_team_1": 1,
+                                  "score_team_2": 1, "round": 1}).sort("date", 1)
+    )
+
+    df = pd.DataFrame(all_games)
+
+    all_teams = db.brazilian_league.distinct("team_2")
+
+    final_df = pd.DataFrame()
+
+    for team in all_teams:
+        team_df = df[df["team_2"] == team].reset_index()
+
+        for idx, row in team_df.iterrows():
+            if row['score_team_2'] > row['score_team_1']:
+                team_df.loc[idx, 'w'] = 1
+            else:
+                team_df.loc[idx, 'w'] = 0
+            team_df.loc[idx + 1, 'w-1'] = team_df.loc[idx, 'w']
+            team_df.loc[idx + 2, 'w-2'] = team_df.loc[idx + 1, 'w-1']
+            team_df.loc[idx + 3, 'w-3'] = team_df.loc[idx + 2, 'w-2']
+            team_df.loc[idx + 4, 'w-4'] = team_df.loc[idx + 3, 'w-3']
+            team_df.loc[idx + 5, 'w-5'] = team_df.loc[idx + 4, 'w-4']
+
+        final_df = pd.concat([final_df, team_df], ignore_index=True)
+
+    final_df = final_df.dropna().drop(columns=['round', 'score_team_1', 'score_team_2', 'team_2']).reset_index()
+
+    X_train, X_test, y_train, y_test = train_test_split(final_df[['w-1', 'w-2', 'w-3', 'w-4', 'w-5']], final_df.w, test_size=0.1)
+
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+
+    model.predict(X_test)
+
+    model.score(X_test, y_test)
+
+    return pd.DataFrame(model.coef_)
--- server/data_science/probabilityofwinninggame.py
+++ server/data_science/probabilityofwinninggame.py
@@ -1,32 +1,50 @@
 import pymongo
 import pandas as pd
-from datetime import datetime
-from app import mongo
+import numpy as np
+from data_science.p_logit_home import probability_home
+from data_science.p_logit_away import probability_away
 
-data = mongo.db.brazilian_league.find({'date': {'$gte': datetime(2019, 1, 1)}})
-df = pd.DataFrame(data=data)
+# Conectar ao Servidor
+myclient = pymongo.MongoClient(
+    "mongodb+srv://write_user:write_pass@futebol-iwbwh.mongodb.net/test?retryWrites=true&w=majority")
+
+# Utilizar o banco de dados dentro do servidor
+db = myclient["futebol"]
+
 
 # Criando Função de Probabilidade do Jogo
-def probability(team_name):
-    # Cria um novo dataframe apenas com o time de interesse
-    new_df = df[df['team_1'] == team_name]
+def probability(team_name_1, team_name_2):
+    # Coletando os 5 últimos jogos do time da casa
+    data = db.brazilian_league.find({'team_1': team_name_1, "score_team_1": {"$ne": "-"}}).sort('date', -1).limit(5)
 
-    # Coloca em ordem do mais antigo para o mais novo a tabela, de acordo com a data do jogo
-    new_df = new_df.sort_values(by='date', ascending=False).reset_index()
+    df_h = pd.DataFrame(data=data)
 
-    # Coletando os 3 últimos jogos
-    wins = 0
-    for i in range(3):
+    for idx, row in df_h.iterrows():
+        if row['score_team_1'] > row['score_team_2']:
+            df_h.loc[idx, 'h'] = 1
+        else:
+            df_h.loc[idx, 'h'] = 0
 
-        if new_df['score_team_1'][i] > new_df['score_team_2'][i]:
-            wins += 1
+    # Coletando os 5 últimos jogos do time da visitante
+    data = db.brazilian_league.find({'team_2': team_name_2, "score_team_2": {"$ne": "-"}}).sort('date', -1).limit(5)
 
-    p1 = 0.2 + 0.2 * wins
-    pe = min(0.6 * p1, 0.16)
-    p2 = 1 - p1 - pe
+    df_a = pd.DataFrame(data=data)
 
-    p1 = round(p1 * 100, ndigits=6)
-    pe = round(pe * 100, ndigits=6)
-    p2 = round(p2 * 100, ndigits=6)
+    for idx, row in df_a.iterrows():
+        if row['score_team_1'] < row['score_team_2']:
+            df_a.loc[idx, 'a'] = 1
+        else:
+            df_a.loc[idx, 'a'] = 0
+
+    ph = probability_home().dot(df_h['h'].to_frame())
+    pa = probability_away().dot(df_a['a'].to_frame())
+    p1 = round(ph.iloc[0, 0], ndigits=6)
+    p2 = round(pa.iloc[0, 0], ndigits=6)
+    pe = round(1 - p1 - p2, ndigits=6)
 
     return p1, pe, p2
+
+
+print(probability('Palmeiras', 'Grêmio'))
+
+pass
